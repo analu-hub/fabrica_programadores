b9fad64e3ae1ee7f73a4cc02a2ad6f53c9b51d6a
analu-hub <annaluizasilvalourenco2027@gmail.com>
2026-05-23 15:59:21 -0300

    Add files via upload

---
+++ 04-python_condicional/atividade09_ifelse.py
@@ -0,0 +1,10 @@
+#Autor:Ana luiza
+#Projeto:Cafeteria Aroma e Sabor
+
+#Definaçao das variaveis
+nome = input("Digite o nome do cliente")
+Pedido = int(input("escolha sua bebida: 1-café ou 2-chá"))
+if Pedido = '1 café'
+    print (nome,'seu cafe esta pronto')
+else:
+    print(nome,'seu chá esta pronto')
