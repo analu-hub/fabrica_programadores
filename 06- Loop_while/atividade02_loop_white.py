9479ba1a690c36ed53afe610ee898c7fbb0f67f3
analu-hub <annaluizasilvalourenco2027@gmail.com>
2026-05-30 16:56:54 -0300

    Add files via upload

---
+++ 06- Loop_while/atividade02_loop_white.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um numero para ver a tabuada:'))
+i = 1
+while i <= 10:
+  print(f'{numero} x {i} = {numero * i}')
+  i = i +1
---
+++ 06- Loop_while/atividade03_loop_white.py
@@ -0,0 +1,7 @@
+numero = int(input('digite a tabuada desejada:'))
+i = int(input('digite o inicio da tabuada:'))
+f = int(input('digite o fim da tabuada:'))
+
+while i <= f:
+    print(f'{numero}x {i} = {numero * i}')
+    i = i + 1
