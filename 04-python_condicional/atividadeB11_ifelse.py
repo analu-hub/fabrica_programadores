be04221d1f7d9db973f91c94934bebbdacfe846e
analu-hub <annaluizasilvalourenco2027@gmail.com>
2026-08-01 15:45:19 -0300

    Python condicionais

---
+++ 04-python_condicional/atividadeB11_ifelse.py
@@ -0,0 +1,10 @@
+nome = input('Digite seu nome')
+idade = input ('Digite sua idade:')
+cnh = input('Possui cnh sim ou nao')
+if idade >= 18:
+    if cnh =='sim'
+    print(f'{nome}) voce pode dirigir')
+    else:
+        print(f'{nome})voce nao pode dirigir
+    else:
+        print(f'{nome})voce e menor de idade
